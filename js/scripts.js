4a5967bdd7ef269ac1289774490994a139a15006
Gabriel Van de Sande <gabe.v@onestepgps.com>
2022-03-28 16:56:46 -0700

    move to main dir

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,46 @@ window.onload = function() {
   
 // Reveal drawing on hover
 
-$('.pic').mousemove(function (event) {
-    event.preventDefault();
-    var upX = event.clientX;
-    var upY = event.clientY;
-    var mask = $('#mask1 circle')[0];
-    mask.setAttribute("cy", (upY - 5) + 'px');
-    mask.setAttribute("cx", (upX) + 'px');
-});
+// $('.pic').mousemove(function (event) {
+//     event.preventDefault();
+//     var upX = event.clientX;
+//     var upY = event.clientY;
+//     var mask = $('#mask1 circle')[0];
+//     mask.setAttribute("cy", (upY - 5) + 'px');
+//     mask.setAttribute("cx", (upX) + 'px');
+// });
 
-$('.btn-join').click(function() {
+$('.btn-join').hover(function() {
    $('.social-container').show()
   });
+
+$('.social-container').mouseleave(function() {
+    $('.social-container').hide()
+});
+
+var firstMute = true;
+var player = document.getElementById("player")
+$('#unmute-btn').click(function() {
+    if(firstMute){
+        player.play();
+        firstMute = false;
+    }else{
+        player.muted = false;
+    }
+    $('#unmute-btn').hide();
+    $('#mute-btn').show();
+})
+
+$('#mute-btn').click(function() {
+        player.muted = true;
+    $('#mute-btn').hide();
+    $('#unmute-btn').show();
+})
+
+
+
+$('#un-mute').click(function() {
+    console.log($('#un-mute').val())
+    player.play();
+})
+
