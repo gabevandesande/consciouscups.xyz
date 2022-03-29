ed684aca14782fa7e68ba662221d32e97b9f0d3b
Gabriel Van de Sande <gabe.v@onestepgps.com>
2022-03-29 15:33:16 -0700

    protective circle, volume move

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,92 @@
-window.onload = function() {
-    if( $(window).height() > $(window).width() )
+// Load screen
+const wait = (delay = 0) =>
+    new Promise(resolve => setTimeout(resolve, delay));
+
+const setVisible = (elementOrSelector, visible) =>
+    (typeof elementOrSelector === 'string'
+        ? document.querySelector(elementOrSelector)
+        : elementOrSelector
+    ).style.display = visible ? 'block' : 'none';
+
+setVisible('.pic', false);
+setVisible('#loading', true);
+
+document.addEventListener('DOMContentLoaded', () =>
+    wait(3000).then(() => {
+        setVisible('.pic', true);
+        setVisible('#loading', false);
+    }));
+
+window.onload = function () {
+    if ($(window).height() > $(window).width())
         $('#under-img').css("height", "100%")
-    else 
+    else
         $('#under-img').css("width", "100%")
-  };
-  
+};
+
 // Reveal drawing on hover
 
-// $('.pic').mousemove(function (event) {
-//     event.preventDefault();
-//     var upX = event.clientX;
-//     var upY = event.clientY;
-//     var mask = $('#mask1 circle')[0];
-//     mask.setAttribute("cy", (upY - 5) + 'px');
-//     mask.setAttribute("cx", (upX) + 'px');
-// });
+$('.pic').mousemove(function (event) {
+    var offset = window.innerWidth * 0.06 + 150
+    var minX = window.innerWidth / 2 - offset;
+    var maxX = window.innerWidth / 2 + offset;
+    var minY = window.innerHeight / 2 - offset - 75;
+    var maxY = window.innerHeight / 2 + offset - 75;
+    console.log(minX, maxX, minY, maxY)
+    event.preventDefault();
+    var mask = $('#mask1 circle')[0];
 
-if ($(window).width() > 820){
-    $('.btn-join').hover(function() {
+    var upX = event.clientX;
+    var upY = event.clientY;
+
+
+    if (upX < minX || upX > maxX || upY < minY || upY > maxY) {
+        $('#maskCircle').fadeIn('fast');
+        mask.setAttribute("cy", (upY - 5) + 'px');
+        mask.setAttribute("cx", (upX) + 'px');
+    } else {
+        $('#maskCircle').fadeOut('fast');
+    }
+
+});
+
+if ($(window).width() > 820) {
+    $('.btn-join').hover(function () {
         $('.social-container').show()
-       });
-     
-     $('.social-container').mouseleave(function() {
-         $('.social-container').hide()
-     });
-} else{
-    $('.btn-join').click(function() {
+    });
+
+    $('.social-container').mouseleave(function () {
+        $('.social-container').hide()
+    });
+} else {
+    $('.btn-join').click(function () {
         $('.social-container-mobile').show()
-       });
+    });
 }
 
 
 var firstMute = true;
 var player = document.getElementById("player")
-$('#unmute-btn').click(function() {
-    if(firstMute){
+$('#unmute-btn').click(function () {
+    if (firstMute) {
         player.play();
         firstMute = false;
-    }else{
+    } else {
         player.muted = false;
     }
     $('#unmute-btn').hide();
     $('#mute-btn').show();
 })
 
-$('#mute-btn').click(function() {
-        player.muted = true;
+$('#mute-btn').click(function () {
+    player.muted = true;
     $('#mute-btn').hide();
     $('#unmute-btn').show();
 })
 
 
 
-$('#un-mute').click(function() {
+$('#un-mute').click(function () {
     console.log($('#un-mute').val())
     player.play();
 })
